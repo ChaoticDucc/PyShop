63a7f8d8caade131edd1dd7741295a5478687653
ChaoticDucc <63168091+ChaoticDucc@users.noreply.github.com>
2024-07-16 15:35:18 +0200

    Added ability to read files
    admin console has been removed

--- store.py
+++ store.py
@@ -1,16 +1,10 @@
 
 import time
 
-class Operation:
-    
-    def __init__(self) -> None:
-        pass
+products_file = "products.txt"
+users_file = "users.txt"
 
-    @classmethod
-    def Debug(cls):
-        print("---------------------- DEBUG ----------------------") 
-        print(Products.categories_dict)
-        print("-------------------- DEBUG END --------------------") 
+class Operation:
 
     # Sets a blank line 100 times
     def ClearTerminal():
@@ -48,58 +42,53 @@ class Operation:
 class Products:
 
     number_of_products = 0
-    id_dict = {}
-    categories_dict = {}
+    product_id_dict = {}
+    product_categories_dict = {}
+
+    def ReadFile():
+        with open(products_file) as file:
+            file_content = file.readlines()
+        for content in file_content:
+            content = content.strip()
+            content = content.split(", ")
+            Products(content[0],content[1],content[2])
 
     def __init__(self, name, price, category) -> None:
+        if Operation.TypeCheck(price, "float") == False:
+            print(f"{products_file} > {name}: Failed to add as product. Price '{price}' is not a number. Skipping...")
+            return
+        self.name = name
+        self.price = float(price)
+        self.category = category
+        if float(price) < 0:
+            print(f"{products_file} > {name}: Negative price '{price}' detected. Is this correct? Continuing...")
         # Tracks the number of products added
         Products.number_of_products += 1
         # Uses number_of_products to assign an ID (self.id) to instance.
         self.id = int(Products.number_of_products)
-        # Adds ID (self.id) as key and instance (self) as value to product dictionary (id_dict).
-        Products.id_dict[self.id] = self
-        # Collects additional information
-        self.name = name
-        self.price = price
-        self.category = category
-        # Adds category (self.category) as key and instance (self) as value to product dictionary (categories_dict).
-        # Checks if self.category is in categories_dict.
-        if self.category in Products.categories_dict:
+        # Adds ID (self.id) as key and instance (self) as value to product dictionary (product_id_dict).
+        Products.product_id_dict[self.id] = self
+        # Adds category (self.category) as key and instance (self) as value to product dictionary (product_categories_dict).
+        # Checks if self.category is in product_categories_dict.
+        if self.category in Products.product_categories_dict:
             # Adds the instance (self) to the list associated with the existing key (self.category).
-            Products.categories_dict[self.category].append(self)
+            Products.product_categories_dict[self.category].append(self)
         else:
             # Creates new key (self.category) and a list as its value, containing the instance (self).
-            Products.categories_dict[self.category] = [self]
+            Products.product_categories_dict[self.category] = [self]
+        print(f"{products_file} > {name}: Added as product '{self.name}', with price '{self.price}' in category '{self.category}'")
 
-    @classmethod
     # Displays a list of all products.
-    def DisplayFullList(cls): 
+    def DisplayFullList(): 
         print("List of Products:")
         print("")
-        for category in Products.categories_dict:
+        for category in Products.product_categories_dict:
             print(category.capitalize())
             print("")
-            for item in Products.categories_dict[category]:
+            for item in Products.product_categories_dict[category]:
                 print(f"[{item.id}] {item.name.capitalize()}")
                 print(f"${item.price:.2f}")
                 print("")
-    
-    @classmethod
-    def AddProduct(cls):
-        while True:
-            new_name = input("Enter Name: ")
-            while True:
-                new_price = input("Enter Price: $")
-                if Operation.TypeCheck(new_price, "float") == True:
-                    break
-                else:
-                    print("Invalid")
-            new_category = input("Enter Category: ")
-            try:
-                Products(new_name, float(new_price), new_category)
-                break
-            except:
-                print("oh oh")
 
 
 class User:
@@ -107,81 +96,80 @@ class User:
     user_dict = {}
     logged_in_user = None
 
-    def __init__(self, username, admin_rights) -> None:
-        if username.islower() == False:
-            raise ValueError("Only lowercase permitted")
-        self.username = username
-        # Adds username (self.username) as key and instance (self) as value to user dictionary (user_dict).
-        User.user_dict[self.username] = self
-        self.admin_rights = admin_rights
+    def ReadFile():
+        with open("users.txt") as file:
+            file_content = file.readlines()
+        for content in file_content:
+            content = content.strip()
+            User(content)
+
+    def __init__(self, name) -> None:
+        if name.islower() == False:
+            print(f"{products_file} > {name}: Uppercase name '{name}' detected. Changing to lowercase... Continuing...")
+        self.name = name.lower()
+        # Adds name (self.name) as key and instance (self) as value to user dictionary (user_dict).
+        User.user_dict[self.name] = self
         self.cart = {}
         self.cart_total = 0
+        print(f"{products_file} > {name}: Added as user '{self.name}'.")
 
-    @classmethod
-    def Login(cls):
-        print("Select a User")
-        for item in User.user_dict.values():
-            print(item.username)
+    def Login():
         while True:
-            username_to_login = input("Enter username: ")
-            if username_to_login not in User.user_dict:
+            user_to_login = input("Enter username: ")
+            if user_to_login not in User.user_dict:
                 print("User not found")
             else: 
-                User.logged_in_user = User.user_dict[username_to_login]
-                print(f"Logged in as {User.logged_in_user.username}")
+                User.logged_in_user = User.user_dict[user_to_login]
+                print(f"Logged in as {User.logged_in_user.name}")
                 Operation.ClearTerminal()
                 break
 
-    @classmethod
-    def LoggedInNow(cls):
-        return(f"Logged in as {User.logged_in_user.username}")
-
 
 class Purchasing(User):
-
-    def __init__(self) -> None:
-        pass
     
     # Receives and validates the input. 
-    def Selection(self): 
+    def Menu(user): 
         Products.DisplayFullList()
         print("Select your product(s) by entering the ID (eg: 1).")
         print("Use 0 to finish.")
         while True:
             print("")
-            user_input_id = input("Product ID: ")
+            user_input = input("Product ID: ")
             # If user enters 0, displays cart and breaks loop.
-            if user_input_id == "0":
+            if user_input == "0":
                 print("Displaying Cart")
-                Purchasing.DisplayCart(self)
+                Purchasing.DisplayCart(user)
                 break
-            if user_input_id == "admin" and self.admin_rights == True:
-                print("epic")
-            # Checks if entered value is an integer.
-            elif Operation.TypeCheck(user_input_id, "int") == False:
-                print("Invalid Input")
-            # Checks if entered value is a key in product dictionary (Products.id_dict).
-            elif int(user_input_id) not in Products.id_dict:
-                print("Not Found")
-            # If none of the above tests have discovered any issues, asks user for quantity. 
             else:
-                # Looks up the instance value (output_instance) in dictionary (Products.id_dict) using key (user_input_id).
-                output_instance = Products.id_dict[int(user_input_id)]
-                print(f"{output_instance.name.capitalize()} selected")
-                print("Use 0 to cancel")
-                while True:
-                    user_input_qty = input("Quantity: ")
-                    # If user enters 0, breaks loop and resets, allowing user to select new item, .
-                    if user_input_qty == "0":
-                        print("Cancelled")
-                        break
-                    # Checks if entered value is an integer
-                    if Operation.TypeCheck(user_input_qty, "int") == False:
-                        print("Invalid Input")
-                    else:
-                        output_qty = int(user_input_qty)
-                        Purchasing.AddToCart(self, output_instance, output_qty)
-                        break
+                Purchasing.ProductSelection(user, user_input)
+
+
+    def ProductSelection(user, requested_id): 
+        # Checks if entered value is an integer.
+        if Operation.TypeCheck(requested_id, "int") == False:
+            print("Invalid Input")
+        # Checks if entered value is a key in product dictionary (Products.product_id_dict).
+        elif int(requested_id) not in Products.product_id_dict:
+            print("Not Found")
+        # If none of the above tests have discovered any issues, asks user for quantity. 
+        else:
+            # Looks up the instance value (output_instance) in dictionary (Products.product_id_dict) using key (user_input_id).
+            product = Products.product_id_dict[int(requested_id)]
+            print(f"{product.name.capitalize()} selected")
+            print("Use 0 to cancel")
+            while True:
+                user_input_qty = input("Quantity: ")
+                # If user enters 0, breaks loop and resets, allowing user to select new item, .
+                if user_input_qty == "0":
+                    print("Cancelled")
+                    break
+                # Checks if entered value is an integer
+                if Operation.TypeCheck(user_input_qty, "int") == False:
+                    print("Invalid Input")
+                else:
+                    qty = int(user_input_qty)
+                    Purchasing.AddToCart(user, product, qty)
+                    break
     
     # Adds a given number (qty_to_add) of given items (item_to_add) to cart.
     def AddToCart(self, item_to_add, qty_to_add):
@@ -200,8 +188,6 @@ class Purchasing(User):
  
     def DisplayCart(user):
         Operation.ClearTerminal()
-        print(User.LoggedInNow())
-        print("")
         print("Cart:") 
         print("")
         # Checks if the cart is empty. An empty list is false. 
@@ -211,43 +197,20 @@ class Purchasing(User):
             for item in user.cart:
                 item_quantity = user.cart[item]
                 print(f"{item.name.capitalize()}")
-                print(f"{item_quantity}x ${item.price:.2f}")
-                print(f"                ${(item.price*item_quantity):.2f}")
+                print(f"{item_quantity}x $ {item.price:.2f}")
+                print(f"                $ {(item.price*item_quantity):.2f}")
             print("_____________________")
             print(f"Total: $ {user.cart_total:.2f}")
         Operation.Pause(0)
 
-class Admin():
-
-    def __init__(self) -> None:
-        pass
-    
-    def PasswordCheck(self):
-        print("Admin Console")
-        entered_password = input("Enter Password: ")
-
-# Class Instances
-
-Products("apple", 0.50, "fruit")
-Products("banana", 0.70, "fruit")
-Products("pretzel", 0.80, "snack")
-Products("muffin", 2.00, "snack")
-Products("chips", 1.50, "snack")
-Products("cola", 2.70, "drink")
-Products("orange juice", 2.50, "drink")
-Products("water", 1.20, "drink")
-
-User("chaoticducc",True)
-User("steve",False)
-
-
 # Operating Code
 
+Products.ReadFile()
+User.ReadFile()
+Operation.ClearTerminal()
 User.Login()
 print(f'''
 ---------------------------------
 Welcome to the School Store!
-{User.LoggedInNow()}
 ---------------------------------''')
-Products.AddProduct()
-Purchasing.Selection(User.logged_in_user)
+Purchasing.Menu(User.logged_in_user)
